1bf288e8bdd693f22ebd82b9b9b099cdcd96cfa7
Heitor da Silva Martind de Carvalho <sirh.dev@gmail.com>
2026-09-24 16:50:43 -0300

    funcao forca bruta

--- forca_bruta.py
+++ forca_bruta.py
@@ -14,3 +14,21 @@ import time
 
 def gerar_hash(senha):
     return hashlib.sha256(senha.encode("utf-8")).hexdigest()
+
+
+def forca_bruta(hash_alvo, alfabeto, tamanho_maximo):
+    """Testar todas as combincoes do alfabeto, de 1 ate tamanho_maimo.
+    
+    Devolve(senha, tentativas) se achar, ou (None, tentativa).
+    """
+    tentativa = 0
+
+    for tamanho in range(1, tamanho_maximo + 1):
+        for combinacao in itertools.product(alfabeto, repeat=tamanho):
+            senha = "".join(combinacao)
+            tentativas += 1
+
+            if gerar_hash(senha) == hash_alvo:
+                return senha, tentativa
+
+    return None, tentativas
