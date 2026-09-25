375fce3787e758996ac3ccf5d95d82890a60d074
Heitor da Silva Martind de Carvalho <sirh.dev@gmail.com>
2026-09-25 14:41:10 -0300

    etapa 5 finalizada

--- forca_bruta.py
+++ forca_bruta.py
@@ -21,7 +21,7 @@ def forca_bruta(hash_alvo, alfabeto, tamanho_maximo):
     
     Devolve(senha, tentativas) se achar, ou (None, tentativa).
     """
-    tentativa = 0
+    tentativas = 0
 
     for tamanho in range(1, tamanho_maximo + 1):
         for combinacao in itertools.product(alfabeto, repeat=tamanho):
@@ -29,6 +29,24 @@ def forca_bruta(hash_alvo, alfabeto, tamanho_maximo):
             tentativas += 1
 
             if gerar_hash(senha) == hash_alvo:
-                return senha, tentativa
+                return senha, tentativas
 
     return None, tentativas
+
+if __name__ == "__main__":
+    ALFABETO = string.ascii_lowercase + string.digits
+    print("Alfabeto: {} caracteres possiveis.".format(len(ALFABETO)))
+    print("")
+
+    ALVOS = ["ab", "cor", "gato", "sol42" "4225@!"]
+
+    for alvo in ALVOS:
+        hash_alvo = gerar_hash(alvo)
+
+        inicio = time.perf_counter()
+        senha, tentativas = forca_bruta(hash_alvo, ALFABETO, tamanho_maximo=6)
+        duracao = time.perf_counter() - inicio
+
+        print("senha '{}' ({} caracteres):".format(alvo, len(alvo)))
+        print("     quebra em {:.3f}s apos  {:,} tentativas".format(duracao, tentativas))
+        print("")
